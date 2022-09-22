3a6af4da6db84cd58467d87195d166ca171a37b1
Matt Welke <mattwelke@gmail.com>
2022-09-21 22:53:48 -0400

    Add FROM header - it's nice to be nice

--- scraping.go
+++ scraping.go
@@ -10,15 +10,21 @@ import (
 
 // Fetches data from the URL.
 func fetchData() (string, error) {
-	resp, err := http.Get(URL)
+	req, _ := http.NewRequest(http.MethodGet, URL, nil)
+	req.Header.Set("FROM", "https://github.com/mattwelke/charter-challenge-for-fair-voting-bot")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		return "", fmt.Errorf("error during HTTP request: %w", err)
 	}
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("could not get body of response: %w", err)
 	}
+
 	return string(body), nil
 }
 
